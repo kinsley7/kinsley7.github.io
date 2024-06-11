360f379b66821f092466bf79f13757bb9824f1a1
kinsley crowdis <kinsleycrowdis@gmail.com>
2024-06-11 10:37:47 -0400

    window card creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,34 +3,19 @@ import { Button } from "@/components/foundation/Button";
 import { Accordion } from "@/components/foundation/Accordion";
 import ChevronUp from "../../public/chevron-up.svg";
 import ChevronDown from "../../public/chevron-down.svg";
-import { Card } from "@/components/foundation/Card";
-import { Row } from "@/components/foundation/Row";
-import hellokitty from "../../public/hello-kitty.svg"
-import { Column } from "@/components/foundation/Column";
+import { WindowCard } from "@/components/compound/WindowCard";
+
 
 //homepage
 export default function Home() {
   return (
     <main>
 
-      {/* window card */}
-      <Card type='elevated' classNames="border-[1px] border-black rounded-[14px]">
-        <Card classNames="border-b-[1px] border-black rounded-t-[14px]">
-          <Row classNames="justify-center">
-            <p> Card Title test </p>
-            <Image src={hellokitty} alt='' className="w-[32px] self-end"/>
-          </Row>
-        </Card>
-        <Column classNames="p-4">
-          <p>Card body testing</p>
-          <h1> THIS IS THE CARD BODY </h1>
-          <h2> other things can go in here!</h2>
-        </Column>
-      </Card>
-
-
-      <Button label='Testing'/>
-      <Accordion title="test" body="testing open" openIcon={<Image src={ChevronDown} alt='chevron down' className="w-5"/>} closeIcon={<Image src={ChevronUp} alt='Chevron up' className="w-5" />}/>
+      <WindowCard classNames="m-2" title='hi'> hi
+        <Button label='Testing'/>
+        <Accordion title="test" body="testing open" openIcon={<Image src={ChevronDown} alt='chevron down' className="w-5"/>} closeIcon={<Image src={ChevronUp} alt='Chevron up' className="w-5" />}/>
+      </WindowCard>
+      
     </main>
   );
 }
---
+++ src/components/compound/WindowCard.tsx
@@ -0,0 +1,36 @@
+import { IComponent } from "../IComponent";
+import { Card } from "@/components/foundation/Card";
+import { Row } from "@/components/foundation/Row";
+import hellokitty from "../../../public/hello-kitty.svg"
+import { Column } from "@/components/foundation/Column";
+import Image from "next/image";
+import { Children, ReactNode, cloneElement, useEffect, useRef, useState } from "react";
+
+//title needs to change on scroll when a new section is reached
+interface WindowCardProps extends IComponent{
+	//content : WindowCardSection[]
+	title : ReactNode;
+	children : ReactNode;
+}
+
+/* interface WindowCardSection {
+	title : ReactNode;
+	children : ReactNode;
+} */
+
+export const WindowCard = ({classNames = ''.trim(), title, children }:WindowCardProps) => {
+	
+	return <div className={classNames}> 
+				<Card type='elevated' classNames="border-[1px] border-black rounded-md">
+					<Card classNames="border-b-[1px] border-black rounded-md">
+						<Row classNames="justify-center px-4">
+							<h1 className="mx-auto">{title}</h1>
+							<Image src={hellokitty} alt='' className="justify-self-end w-[30px]"/>
+						</Row>
+					</Card>
+					<Column classNames="p-4 overflow-y-auto">
+						{children}
+					</Column>
+				</Card>
+			</div>
+}
