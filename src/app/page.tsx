777da9842b4116fb9d21464d4331b6cb021436b0
kinsley crowdis <kinsleycrowdis@gmail.com>
2024-06-25 11:17:58 -0400

    external icon is good just cant change color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import ChevronDown from "../../public/chevron-down.svg";
 import { MainWindowCard } from "@/components/compound/MainWindowCard";
 import { Spacer } from "@/components/foundation/Spacer";
 import { NavigationCard } from "@/components/compound/NavigationCard";
+import { Link } from "@/components/foundation/Link";
 
 //homepage
 export default function Home() { 
@@ -27,7 +28,7 @@ export default function Home() {
             <section id='home' className="mb-700">
                 Reached homepage section
                 <br/>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, <span className="font-bold">sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <Link type="external" url="https://www.youtube.com">this is an external link</Link>  Lorem ipsum dolor sit amet, consectetur adipiscing elit, <span className="font-bold">sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </span> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 <Button label='Home'/>
                 <Spacer direction='vertical' size={35}/>
                 <section id='about me'>
--- src/components/foundation/Link.tsx
+++ src/components/foundation/Link.tsx
@@ -1,13 +1,31 @@
 //internal or external links
 
+import { ReactHTMLElement, ReactNode } from "react";
 import { IComponent } from "../IComponent";
+import { Row } from "./Row";
+import Image from "next/image";
+import externalLink from "../../../public/external-link.svg"
 
 interface LinkProps extends IComponent {
 	type : 'internal' | 'external'
+	url ?: string
+	children : string
+	//onClick? 
 }
 
 //if link is internal then use this svg in the ::after. if its external than a different svg
 
-export const Link = ({} : LinkProps) => {
-	
+
+//<Image classNames='w-[20px] p-1' src={externalLink} alt='external image'/>
+export const Link = ({type, url, children} : LinkProps) => {
+	if(type = 'external'){
+		return(
+				<span><a target="_blank" href={url} className="link">{children}</a></span>
+		)
+	}
+	else{
+		return(
+			<span><a>{children}</a></span>
+			)
+	}
 }
